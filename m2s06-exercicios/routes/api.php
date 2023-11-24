de2818a06497c0965c450244928a2a7b75e59b7c
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-11-24 13:51:52 -0300

    Criando Rota e Método para Listar todas as pessoas

--- m2s06-exercicios/routes/api.php
+++ m2s06-exercicios/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PessoasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*Agrupamento de rotas */
+Route::resource('pessoas', PessoasController::class)
+    ->only(['index', 'show', 'store', 'update', 'destroy']);
